fd57d15e2389aedf6cad8524a74e25254b7433f4
marek z <marek127@gmail.com>
2016-12-05 10:46:14 -0800

    Add auction and bid model/controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  resources :auctions, shallow: true do
+  	resources :bids, only: [:create, :destroy, :update]
+  end
+
+
+
 end
